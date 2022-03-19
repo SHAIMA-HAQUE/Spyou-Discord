d98e338b474bbe129bccd7895ed980f8a2a20fbf
Shaima Haque <shaimahaque02@yahoo.com>
2022-03-19 17:22:26 +0530

    Infinite loop solved

--- index.js
+++ index.js
@@ -37,14 +37,18 @@ client.on('interactionCreate', async interaction => {
 });
 
 
-client.on("message", msg => {
+client.on("messageCreate", msg => {
+	if (msg.author.bot) { return; }
+	else{
+    msg.reply("Ping!") 
+	   }// with mention
+});
     // var message = new MessageEmbed()
     //   .setDescription("Pong") // sets the body of it
     //   .setColor("#00ff00") // sets the color of the embed
     //   .setThumbnail("https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn.pixabay.com%2Fphoto%2F2015%2F04%2F23%2F22%2F00%2Ftree-736885__480.jpg&imgrefurl=https%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Fnature%2F&tbnid=DH7p1w2o_fIU8M&vet=12ahUKEwjIkLHuhdL2AhVmyXMBHQ1cBdIQMygBegUIARDWAQ..i&docid=Ba_eiczVaD9-zM&w=771&h=480&q=images&ved=2ahUKEwjIkLHuhdL2AhVmyXMBHQ1cBdIQMygBegUIARDWAQ") // sets the thumbnail of the embed
     //   .setTitle("This is an embed")
     // //msg.channel.send("Ping!") // without mention
-    msg.reply("Ping!") // with mention
-})
+	
 
 client.login(token);
