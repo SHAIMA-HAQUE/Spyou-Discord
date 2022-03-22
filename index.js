f2f49d1dcb9e7fdb3af964ad16ced19005490e02
Shaima Haque <shaimahaque02@yahoo.com>
2022-03-22 15:30:06 +0530

    Bot working for hard coded message

--- index.js
+++ index.js
@@ -12,39 +12,39 @@ const client = new Client({
 
 // The minimum prediction confidence.
 const threshold = 0.9;
-
+let model;
 // Load the model. Users optionally pass in a threshold and an array of
 // labels to include.
-toxicity.load(threshold).then(model => {
+
   const sentences = ['you suck'];
 
-  model.classify(sentences).then(predictions => {
-    // `predictions` is an array of objects, one for each prediction head,
-    // that contains the raw probabilities for each input along with the
-    // final prediction in `match` (either `true` or `false`).
-    // If neither prediction exceeds the threshold, `match` is `null`.
+//   model.classify(sentences).then(predictions => {
+//     // `predictions` is an array of objects, one for each prediction head,
+//     // that contains the raw probabilities for each input along with the
+//     // final prediction in `match` (either `true` or `false`).
+//     // If neither prediction exceeds the threshold, `match` is `null`.
 
-    console.log(predictions);
-    /*
-    prints:
-    {
-      "label": "identity_attack",
-      "results": [{
-        "probabilities": [0.9659664034843445, 0.03403361141681671],
-        "match": false
-      }]
-    },
-    {
-      "label": "insult",
-      "results": [{
-        "probabilities": [0.08124706149101257, 0.9187529683113098],
-        "match": true
-      }]
-    },
-    ...
-     */
-  });
-});
+//     console.log(predictions);
+//     /*
+//     prints:
+//     {
+//       "label": "identity_attack",
+//       "results": [{
+//         "probabilities": [0.9659664034843445, 0.03403361141681671],
+//         "match": false
+//       }]
+//     },
+//     {
+//       "label": "insult",
+//       "results": [{
+//         "probabilities": [0.08124706149101257, 0.9187529683113098],
+//         "match": true
+//       }]
+//     },
+//     ...
+//      */
+//   });
+// });
 
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) return;
@@ -70,15 +70,21 @@ for (const file of commandFiles) {
 	client.commands.set(command.data.name, command);
 }
 
-client.once('ready', () => {
+client.once('ready', async() => {
+	model = await toxicity.load(threshold);
 	console.log('Ready!');
 });
 
 
-client.on("messageCreate", msg => {
+client.on("messageCreate", async msg => {
 	if (msg.author.bot) { return; }
 	else{
-    msg.reply("Ping!") 
+		let predictions = await model.classify(sentences);
+		predictions.forEach(prediction => {
+			if(prediction.results[0].match){
+				msg.reply("Warning!");
+			}
+		}); 
 	   }// with mention
 });
     // var message = new MessageEmbed()
